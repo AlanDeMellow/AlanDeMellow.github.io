33ea1bf2e3724e116bd41fef04b1b847e7372a59
Alan Gabriel de Mello <210317@upf.br>
2025-06-26 19:26:31 -0300

    first commit

---
+++ js/script.js
@@ -0,0 +1,23 @@
+function alternarTema() {
+    document.body.classList.toggle('light-mode');
+}
+
+function acessibilidade() {
+    const body = document.body;
+    const style = window.getComputedStyle(body);
+    const atual = parseFloat(style.fontSize);
+
+    if (atual < 24) {
+        body.style.fontSize = (atual + 2) + 'px';
+    }
+}
+
+function diminuirFonte() {
+    const body = document.body;
+    const style = window.getComputedStyle(body);
+    const atual = parseFloat(style.fontSize);
+
+    if (atual > 12) { // define um limite mínimo
+        body.style.fontSize = (atual - 2) + 'px';
+    }
+}
